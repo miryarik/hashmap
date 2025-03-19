96c69d73a9f3caa560e058d884024abd780ed7b7
miryarik <85996187+miryarik@users.noreply.github.com>
2025-03-19 19:11:00 +0530

    Added set

---
+++ hashmap.js
@@ -0,0 +1,54 @@
+class HashMap {
+    #table;
+
+    constructor(loadFactor, capacity) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        this.#table = new Array(capacity);
+
+        // all buckets are linked lists
+        for(let i = 0; i < capacity; i++) {
+            this.#table[i] = new LinkedList();
+        }
+    }
+
+    set(key, value) {
+        // find corresponding bucket
+        const bucket = this.#table[this.#hash(key)];
+
+        // if key already exists in bucket
+        const idx = bucket.find(key);
+        if (idx) {
+            // if yes, then overwrite its value
+            bucket.at(idx).data.key = value;
+        }
+        else {
+            // if not then put the key-value in bucket
+            bucket.append({key, value});
+        }
+
+    }
+
+    print() {
+        console.log(this.#table);
+    }
+
+    #hash(key) {
+        // basic input type check
+        if (typeof key != "string") throw Error("Only string types are supported");
+
+        let hashCode = 0;
+        
+        // basic hash function
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+
+            // keep it within table length
+            hashCode = hashCode % this.capacity;
+        }
+
+        return hashCode;
+    }
+}
+
